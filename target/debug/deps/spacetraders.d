8597ac93c33a45242734e14ca1fba07979d59710
RemusW <rwong2100@hotmail.com>
2023-05-09 01:46:18 -0500

    first

---
+++ target/debug/deps/spacetraders.d
@@ -0,0 +1,5 @@
+D:\Code workshop\Rust\spacetraders\target\debug\deps\spacetraders.exe: src\main.rs
+
+D:\Code workshop\Rust\spacetraders\target\debug\deps\spacetraders.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/spacetraders.d
@@ -0,0 +1,1 @@
+D:\Code\ workshop\Rust\spacetraders\target\debug\spacetraders.exe: D:\Code\ workshop\Rust\spacetraders\src\main.rs
