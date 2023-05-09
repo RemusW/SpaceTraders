7653a4d3ef824cd6880d807949bc6c808ac44dbe
RemusW <rwong2100@hotmail.com>
2023-05-09 01:52:17 -0500

    gitignore

--- target/debug/deps/spacetraders-0432ae198dd37915.d
+++
@@ -1,5 +0,0 @@
-d:\Code workshop\Rust\spacetraders\target\debug\deps\spacetraders-0432ae198dd37915.rmeta: src\main.rs
-
-d:\Code workshop\Rust\spacetraders\target\debug\deps\spacetraders-0432ae198dd37915.d: src\main.rs
-
-src\main.rs:
--- target/debug/deps/spacetraders-68e90068142f6e42.d
+++
@@ -1,5 +0,0 @@
-d:\Code workshop\Rust\spacetraders\target\debug\deps\spacetraders-68e90068142f6e42.rmeta: src\main.rs
-
-d:\Code workshop\Rust\spacetraders\target\debug\deps\spacetraders-68e90068142f6e42.d: src\main.rs
-
-src\main.rs:
